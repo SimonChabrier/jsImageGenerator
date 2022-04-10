baf9af5ee36c745b46a04031daee32bddba761a4
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-10 03:41:41 +0200

    add features to generate adaptative pictures and stop append picture by window size

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const app = {
     init: function ()
     { 
         console.log('appinit');
-        app.appendImage()
+        app.appendImage();
     },
 
     //number generator
@@ -12,32 +12,52 @@ const app = {
         return Math.floor(Math.random() * 100);
     },
 
+    //pic size adaptation
+    requestPictureSize : function (){
+        
+        // request image size in terms of user current window size.
+        let imgHeight = Math.round((( window.innerHeight)));
+        // check curent user screen resolution
+        let screenWidth = window.screen.width * window.devicePixelRatio ;
 
-     
+        // set a query proportionate image height in % of curent user screen resolution
+        if (screenWidth >= 1920){
+            imgHeight = Math.round(imgHeight * 55 / 100);
+            console.log('screen up than 1920px : request img size = ' + imgHeight)
+        }
+
+        if (screenWidth < 1919){
+            imgHeight = Math.round(imgHeight * 30 / 100);
+            console.log('screen less than 1920px : request img size =  ' + imgHeight)
+        }
+
+        return imgHeight
+    },
+
     //pic generator 
     imageGenerateur: function () 
     {   
-        // first I create an img element
+        //init new empty image tag
         let img = document.createElement('img');
-        // request image size in terms of user current window size.
-        let imgHeight = ((15 * window.innerHeight) / 100);
-        roudImgHeight = Math.round(imgHeight);
-        console.log(Math.round(imgHeight))
-        
-        img.src = 'https://picsum.photos/' + roudImgHeight + '/?random=' + app.randomNumber()
+        //insert link in this new empty image tag
+        img.src = 'https://picsum.photos/' + app.requestPictureSize() + '/?random=' + app.randomNumber();
+
+        //set start picture styles diplay state
         img.style.opacity = '0'
         img.style.width = '0'; 
         img.style.borderRadius = '100%'; 
-        img.style.transition = '2.5s';
-
-        //final picture styles diplay state
+        img.style.transition = '5s';
+        
+        //set end picture styles diplay state
         setInterval(function(){   
         img.style.width = '';   
-        img.style.opacity = '1'  
+        img.style.opacity = '1';  
         img.style.borderRadius = ''; 
-        }, 1000) //transition intervals
+        img.style.width = 10 + '%'; 
+        
+        }, 1000)
 
-        // end return img each new element
+        // return each new img element and style properties
         return img; 
 
     },
@@ -45,44 +65,47 @@ const app = {
     //append img generator 
     appendImage: function () 
     {       
-            //* actually we don't use counter cause we clear actions intervale using 'offsetHeight'
-            //let counter = 0; // I set a counter
+
+            let windowHeight = window.innerHeight; //get user window current height
 
             let Interval = setInterval(function(){ // I set a time interval between each action
-            //counter++;// increment couter on each new interval action
 
-            app.OnMouseOver(); // call my custom style function
-            let imageInsert = document.getElementById("insert"); 
-            
-            let img = app.imageGenerateur(); // call my image generator function to grab each new generated picture
-            imageInsert.append(img); // And I happend each generated picture
-               
-            let divPictWrapper = document.getElementById('wrapper');
-            let currentHeightPictWrapper = divPictWrapper.offsetHeight;
-            let windowHeight = window.innerHeight; //hauteur de l'écran
+            let imageInsert = document.getElementById('insert'); 
+            let divSize = imageInsert.offsetHeight
 
-            //if(counter === 10) { // use it if you whant to use counter and uncomment let couter ans couter++ lines
+            if(divSize < windowHeight) { 
 
-            if(currentHeightPictWrapper >= windowHeight) { 
-                console.log(currentHeightPictWrapper >= windowHeight)
-                clearInterval(Interval); // then I stop actions and stop add pictures
-            } 
-        }, 400) //timming between actions in ms
+                let img = app.imageGenerateur(); // call my image generator function to grab each new generated picture
+                imageInsert.append(img); // Happend each new generated picture
+                
+                app.OnMouseOver(); // call my custom style function
+  
+            } else (
+                
+                clearInterval(Interval)
+            )
+           
+        }, 380) //timming between insert actions in ms
                                                                                                                             
     },     
 
-    //style generator                                                                                                                                                                                                                          
+    //style generator on currentTarget                                                                                                                                                                                                                      
     OnMouseOver: function ()
     {
         let imgList = document.querySelectorAll('img');                        
         for (let i = 0; i < imgList.length; i++) {   
 
             imgList[i].addEventListener("mouseover", function(event) {                                                            
-                event.target.style.filter = "grayscale(100%)";      
+                event.target.style.filter = "grayscale(100%)";   
+                event.target.style.width = 20 + '%';   
+                event.target.style.borderRadius = 100 + '%';
+                event.target.style.transition = "1.2s"; 
             })
 
             imgList[i].addEventListener("mouseout", function(event) {                                                        
-                event.target.style.filter = "grayscale(0%)";                                                                                                                                                                
+                event.target.style.filter = "grayscale(30%)"; 
+                event.target.style.borderRadius = ""; 
+                event.target.style.transition = "8s";                                                                                                                                                            
             })
         }   
     },
